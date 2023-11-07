64cc3cce34a6d6b3828d25c7ac75bb69611bacdd
iFrodo <igor@azinet.ru>
2023-11-07 11:18:01 +0300

    3d task

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -1,6 +1,11 @@
 let number1 = Math.floor(Math.random() * 100)
 if(number1 % 2 == 0){
-    console.log(`'[${number1}]:Это четное число'`)
+    console.log(`'[${number1}]:Это четное число'`);
 }else console.log(`'[${number1}]:Это нечетное число'`)
 
 
+let number = 1;
+console.log(number++);
+console.log(++number);
+console.log(number--);
+console.log(--number);
