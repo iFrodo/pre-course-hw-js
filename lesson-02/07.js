2beefd5adc0c8278c31968dddeebe50342647276
iFrodo <igor@azinet.ru>
2023-11-14 16:34:04 +0300

    week-2_task-7

--- lesson-02/07.js
+++ lesson-02/07.js
@@ -1,12 +1,12 @@
 let passportMarried = {
     name: "Petr",
     surname: "Petrov",
-    address: {
+    adress: {
         country: "USA",
         city: "Bobryisk"
     }
 };
 
-let passportMarriedCopy = {  name: passportMarried.name, surname: passportMarried.surname,adress: { ...passportMarried.address }, married: true }
+let passportMarriedCopy = {  name: passportMarried.name, surname: passportMarried.surname,adress: { ...passportMarried.adress }, married: true }
 console.log(passportMarried)
 console.log(passportMarriedCopy)
